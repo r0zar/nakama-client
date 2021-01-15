6797babee3a45505050dbe05a5dd4e0edca3243e
Ross Ragsdale <ross.ragsdale@gmail.com>
2021-01-15 00:01:43 -0600

    Debug.

--- pages/api/webhooks.js
+++ pages/api/webhooks.js
@@ -41,13 +41,13 @@ const relevantEvents = new Set([
 ]);
 
 const webhookHandler = async (req, res) => {
-  console.log(req);
   if (req.method === 'POST') {
     const buf = await buffer(req);
     const sig = req.headers['stripe-signature'];
     const webhookSecret =
       process.env.STRIPE_WEBHOOK_SECRET_LIVE ??
       process.env.STRIPE_WEBHOOK_SECRET;
+    console.log(sig, webhookSecret);
     let event;
 
     try {
