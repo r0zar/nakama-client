f65561a612ba66e789cce10f319c7d15d227f348
Ross Ragsdale <ross.ragsdale@gmail.com>
2021-02-06 16:46:29 -0600

    Format text.

--- pages/about.js
+++ pages/about.js
@@ -1,26 +1,33 @@
 const About = () => {
-  const text = `
+  const text1 = `
     Hi I'm Ross 👋
-
+  `
+  const text2 = `
     handle.events has a simple mission to make working with websockets more accessable.
-
+  `
+  const text3 = `
     Creating a persistant connection to a websocket host requires a server to run 24/7,
     so there arn't many serverless options to accomplish that goal. I looked for a no-code
-    solution for this problem I didn't find out, so I made build it. 
-    
+    solution for this problem I didn't find out, so I made build it.
+  `
+  const text4 = `    
     Give handle.events a try and let me know what you think. 
     I'm working hard to add support for more websocket hosts and event handlers,
     so if you have any suggestions, I'd love to hear them!
-
+  `
+  const text5 = `
     Thanks,
     Ross Ragsdale
-
   `
   return (
     <div className="w-auto p-3 max-w-2xl m-auto my-64">
       <div className="flex flex-col justify-center pb-12 ">
         <h1 className="text-2xl my-4">Privacy Policy</h1>
-        <p>{text}</p>
+        <p>{text1}</p>
+        <p>{text2}</p>
+        <p>{text3}</p>
+        <p>{text4}</p>
+        <p>{text5}</p>
       </div>
     </div>
   )
